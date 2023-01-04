eaf137a0ecc29b01ac4b761484415fb2f473948f
Your Name <uzcointg@gmail.com>
2023-01-04 19:48:09 +0500

    some changes

--- js/script.js
+++ js/script.js
@@ -1,39 +1,26 @@
+AOS.init();
+
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
-let sections = gsap.utils.toArray(".panel");
-let cards = gsap.utils.toArray(".card");
+let cards = gsap.utils.toArray('.about_card');
 
-gsap.to(sections, {
-    xPercent: -100 * (sections.length - 1),
-    ease: "none",
-    scrollTrigger: {
-        trigger: ".horizont",
-        pin: true,
-        scrub: 1,
-        // snap: 1 / (sections.length - 1),
-        end: () => "+=" + document.querySelector(".horizont").offsetWidth,
-    }
-});
+// gsap.from(cards, {
+//     opacity: 0,
+//     y: 100,
+//     duration: 1,
+//     stagger: 0.2,
+//     autoAlpha: 0,
+//     scrollTrigger: {
+//         trigger: '.about',
+//         start: 'top bottom'
+//     }
+// });
 
-gsap.from(cards, {
-    opacity: 0,
-    y: 100,
-    duration: 1,
-    stagger: 0.2
-});
-
-const title = document.querySelector(".about__title");
-const text = document.querySelector(".about__text");
-const splitText = new SplitText(text, { type: "words" });
+const title = document.querySelectorAll(".header_logo_title");
+const splitText = new SplitText(title, { type: "lines, words" });
 gsap.set(title, { perspective: 400 });
 
-splitText.split({ type: "lines" });
-gsap.from(splitText.lines, {
-    scrollTrigger: {
-        trigger: '#about',
-        start: "left center",
-        markers: true
-    },
+gsap.from(splitText.words, {
     duration: 0.5,
     opacity: 0,
     rotationX: -100,
