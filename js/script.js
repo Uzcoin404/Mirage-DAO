20e8d4d0b7a4fdeb2c34866be6a3085094ba4e3d
Your Name <uzcointg@gmail.com>
2023-01-25 15:07:22 +0500

    some changes

--- js/script.js
+++ js/script.js
@@ -5,25 +5,32 @@
 //     document.querySelector(".loader-bg").style.display = "none";
 // }, 2000);
 
-const typedTextSpan = document.querySelector(".header_logo_title");
+const typedText = document.querySelector(".header_logo_title");
+const typedText2 = document.querySelector(".header_logo_text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Mirage DAO"];
+const textArray = [typedText.getAttribute("data-typing")];
+const text2 = typedText2.getAttribute("data-typing");
 const typingDelay = 150;
 const erasingDelay = 100;
 const newTextDelay = 1; // Delay between current and next text
 let textArrayIndex = 0;
 let charIndex = 0;
+let charIndex2 = 0;
 
 function type() {
     if (charIndex < textArray[textArrayIndex].length) {
         if (!cursorSpan.classList.contains("typing"))
             cursorSpan.classList.add("typing");
-        typedTextSpan.textContent +=
-            textArray[textArrayIndex].charAt(charIndex);
+        typedText.textContent += textArray[textArrayIndex].charAt(charIndex);
         charIndex++;
         setTimeout(type, typingDelay);
     } else {
+        if (charIndex < text2.length) {
+            typedText2.textContent += text2.charAt(charIndex2);
+            charIndex2++;
+            setTimeout(type, 75);
+        }
         cursorSpan.classList.remove("typing");
         // setTimeout(erase, newTextDelay);
     }
@@ -33,7 +40,7 @@ function erase() {
     if (charIndex > 0) {
         if (!cursorSpan.classList.contains("typing"))
             cursorSpan.classList.add("typing");
-        typedTextSpan.textContent = textArray[textArrayIndex].substring(
+        typedText.textContent = textArray[textArrayIndex].substring(
             0,
             charIndex - 1
         );
@@ -47,7 +54,8 @@ function erase() {
     }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
+window.addEventListener("load", function () {
     // On DOM Load initiate the effect
-    if (textArray.length) setTimeout(type, newTextDelay + 250);
+    if (typedText) type();
+    if (typedText2) setTimeout(type2, 1500);
 });
